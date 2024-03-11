52ed0a9c237790558320fb2bab2a97bd1af373d4
yushulx <lingxiao1002@gmail.com>
2024-03-11 13:57:59 +0800

    Updated README.md and js code

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Online Document Scanning and Uploading with Dynamic Web TWAIN & Node.js
 
-The sample shows how to implement a simple online document scanning and uploading application using Dynamic Web TWAIN and Node.js.
+This sample demonstrates how to implement a simple online document scanning and uploading application using [Dynamic Web TWAIN](https://www.dynamsoft.com/web-twain/overview/?utm_content=nav-products) and Node.js.
 
-## Screenshots
-![image](http://www.codepool.biz/wp-content/uploads/2014/11/nodejs_dwt.png)
 
 ## Download & Installation
-* [Node.js][1]
+- [Node.js](https://nodejs.org/en/download/)
+- [Express](https://expressjs.com/)
+- [formidable](https://github.com/node-formidable/formidable)
 
 ## How to Run
 1. Install the dependent packages:
@@ -19,8 +19,7 @@ The sample shows how to implement a simple online document scanning and uploadin
 	```bash
 	node server.js
 	```
-3. Visit `localhost:2016` in your browser.
 
-[1]:http://nodejs.org/download/
-[2]:http://expressjs.com/
-[3]:https://github.com/felixge/node-formidable
+3. Open `localhost:2024` in your browser.
+
+    ![image](https://www.dynamsoft.com/codepool/img/2024/03/dynamic-web-twain-nodejs-document-scan.png)
--- server.js
+++ server.js
@@ -3,32 +3,30 @@ var util = require('util');
 var express = require('express');
 var fs = require('fs');
 var app = express();
-app.use(express.static(__dirname)); 
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/Resources')); // load static resources
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-});  
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+	res.header("Access-Control-Allow-Credentials", true);
+	next();
+});
 
 app.post('/upload', function (req, res) {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        // console.log(util.inspect({
-        //     fields: fields,
-        //     files: files
-        // }));
-		
-		fs.readdir(__dirname + "/UploadedImages/", function(err, readfiles) {
-			console.log(err);
-			if (err) {				
-				fs.mkdir(__dirname + "/UploadedImages/", function(err) {
-					console.log('make dir success.');
+	var form = new formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
+		// console.log(util.inspect({
+		//     fields: fields,
+		//     files: files
+		// }));
+
+		fs.readdir(__dirname + "/UploadedImages/", function (err, readfiles) {
+			if (err) {
+				fs.mkdir(__dirname + "/UploadedImages/", function (err) {
 				});
 			}
-			
+
 			fs.readFile(files.RemoteFile.path, function (err, data) {
 				// save file from temp dir to new dir
 				var newPath = __dirname + "/UploadedImages/" + files.RemoteFile.name;
@@ -39,13 +37,13 @@ app.post('/upload', function (req, res) {
 				});
 			});
 		});
- 
-        
-    });
+
+
+	});
 })
 
-var server = app.listen(2016, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('listening at http://%s:%s', host, port);
+var server = app.listen(2024, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('listening at http://%s:%s', host, port);
 })
